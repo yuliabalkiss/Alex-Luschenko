38fcd97243d15b825d854fd86972dbc302fd843e
yuliabalkiss <balkisula451@gmail.com>
2022-10-20 00:38:06 +0300

    feat: add file

--- unit_13/unit_13.js
+++ unit_13/unit_13.js
@@ -283,7 +283,14 @@ let a13 = {
 };
 
 function f13() {
+    let sum = 0;
+    for (let key in a13) {
+        if (typeof a13[key] == 'number') {
+            sum += a13[key];
 
+        }
+    }
+    document.querySelector('.out-13').innerHTML = sum;
 }
 
 document.querySelector('.b-13').onclick = f13;
