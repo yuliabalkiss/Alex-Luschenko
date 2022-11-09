c903b09679fa180d4f01538d19204c8945b91e3f
yuliabalkiss <balkisula451@gmail.com>
2022-11-09 16:55:52 +0300

    change file

--- unit_18/unit_18.js
+++ unit_18/unit_18.js
@@ -34,8 +34,15 @@ document.querySelector('.b-2').onclick = t2;
 // Task 3 ============================================
 /*  Дан массив a3 = [2, 'hello', 3, 'hi', 4, 'Mazai'] - с помощью forEach переберите массив и создайте новый массив a3_res куда добавьте элементы данного массива являющиеся числом. Запускаться решение должно при вызове функции t3.*/
 
+let a3 = [2, 'hello', 3, 'hi', 4, 'Mazai'];
 function t3() {
-
+    a3_res = [];
+    a3.forEach(el => {
+        if (typeof el === 'number') {
+            a3_res.push(el);
+        }
+    });
+    document.querySelector('.out-3').innerHTML = a3_res;
 }
 
 document.querySelector('.b-3').onclick = t3;
