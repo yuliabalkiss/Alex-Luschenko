3cfd01dd90ccd190b96d58dffd77eec8a7cfeca0
yuliabalkiss <balkisula451@gmail.com>
2022-11-09 16:51:12 +0300

    change file

--- unit_18/unit_18.js
+++ unit_18/unit_18.js
@@ -18,10 +18,14 @@ document.querySelector('.b-1').onclick = t1;
  создайте новый массив a2_res куда добавьте элементы данного массива деленные на 2. Действия должны
 запускаться при вызове функции t2.*/
 
-let a2 = [2,3,4,5,10,11,12]
+let a2 = [2, 3, 4, 5, 10, 11, 12]
 
 function t2() {
-
+    a2_res = [];
+    a2.forEach(el => {
+        a2_res.push(el / 2)
+    });
+    document.querySelector('.out-2').innerHTML = a2_res;
 }
 
 document.querySelector('.b-2').onclick = t2;
