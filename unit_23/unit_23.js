0bc61ca03247afb6b303ad716cfebfbb84ef31b7
yuliabalkiss <balkisula451@gmail.com>
2022-11-27 22:54:57 +0300

    change file

--- unit_23/unit_23.js
+++ unit_23/unit_23.js
@@ -90,7 +90,6 @@ function t7() {
     let inputVal = document.querySelector('.i-7').value;
     a7.push(inputVal);
     let a = localStorage.setItem('a7', JSON.stringify(a7));
-    a = JSON.parse(a)
     console.log(a)
     localStorage.setItem('a7', JSON.stringify(a7))
 
