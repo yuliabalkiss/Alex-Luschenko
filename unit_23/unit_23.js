d7f7097a1e167c40440b0145e1a496eed0a0e05d
yuliabalkiss <balkisula451@gmail.com>
2022-11-27 22:49:42 +0300

    change file

--- unit_23/unit_23.js
+++ unit_23/unit_23.js
@@ -82,20 +82,33 @@ document.querySelector('.b-6').addEventListener('click', t6)
 
 // Task 7 ============================================
 /*  Создайте input i-7 куда пользователь может вводить числа и строки. Создайте массив a7. Когда пользователь нажимает кнопку b-7 число должно добавляться в массив. Массив должен сохраняться в LS с ключем a7.*/
-
+let a7 = [];
 function t7() {
+    if (localStorage.getItem('a7')) {
+        a7 = JSON.parse(localStorage.getItem('a7'));
+    }
+    let inputVal = document.querySelector('.i-7').value;
+    a7.push(inputVal);
+    let a = localStorage.setItem('a7', JSON.stringify(a7));
+    a = JSON.parse(a)
+    console.log(a)
+    localStorage.setItem('a7', JSON.stringify(a7))
 
 }
 
-// ваше событие здесь!!!
+document.querySelector('.b-7').addEventListener('click', t7)
 
 // Task 8 ============================================
 /*   Создайте функцию t8 при запуске которой из a7 удаляется последний элемент. После чего массив сохраняется в LS с ключем a7. Использовать массив из предыдущего задания. */
 
 function t8() {
-
+    let a = localStorage.getItem('a7');
+    a = JSON.parse(a)
+    a.pop();
+    localStorage.setItem('a7', JSON.stringify(a7))
+    console.log(a)
 }
 
-// ваше событие здесь!!!
+document.querySelector('.b-8').addEventListener('click', t8)
 
 
