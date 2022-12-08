e74cfdf4c1ba0da43d4a28ce4177a34974a4ac7f
yuliabalkiss <balkisula451@gmail.com>
2022-12-08 23:40:38 +0300

    change file

--- unit_28/Goods.js
+++ unit_28/Goods.js
@@ -1,9 +1,16 @@
 //создание объекта с помощью класса
 class Goods {
     constructor(name, price, image, count) {
+        //определяем свойства 
         this.name = name
         this.price = price
         this.image = image
         this.count = count
+        // определяем метод
+        this.draw = function () {
+            let out = document.createElement('div');
+            document.querySelector('.out-4').append(out);
+            out.classList.add()
+        }
     }
 }
