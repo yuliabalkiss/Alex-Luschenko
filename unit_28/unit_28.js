5ec39a3f7cdc244dd9f9ae01ff33e89267f0e44a
yuliabalkiss <balkisula451@gmail.com>
2022-12-09 00:13:17 +0300

    change file

--- unit_28/unit_28.js
+++ unit_28/unit_28.js
@@ -8,3 +8,7 @@
 // const goods_1 = new Goods('apple', 23.5, 'https://cdn0.iconfinder.com/data/icons/fruity-3/512/Apple-48.png', 400);
 // console.log(goods_1);
 
+// Task 3
+const goods_1 = new Goods('apple', 23.5, 'https://cdn0.iconfinder.com/data/icons/fruity-3/512/Apple-48.png', 400);
+console.log(goods_1);
+document.querySelector('.out-3').append(goods_1.draw());
